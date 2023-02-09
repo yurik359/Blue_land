83a7bd887dfdae8009f2cd92848ee78d0c69b0b6
Yura <yurik52222@gmail.com>
2023-02-09 15:16:47 +0200

    +2parts

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ new Swiper('.swiper',{
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev',
     },
-    slidesPerView:4,
+    
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+        480:{
+            slidesPerView:2,
+        },
+        992:{
+            slidesPerView:3,
+        },
+        1200:{
+            slidesPerView:4,
+        },
+    }
 });
